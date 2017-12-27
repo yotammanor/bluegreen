ce2443fafa4d20a7becb5274cc5b73971d62be60
Yotam Manor <yotammanor@gmail.com>
2017-12-27 15:10:02 -0500

    remove deprecated field and the wrappers.

--- core/managers.py
+++ core/managers.py
@@ -4,6 +4,4 @@ from core.blue_green_utils import is_migration_applied
 
 class ChapterManager(models.Manager):
     def get_queryset(self):
-        if is_migration_applied():
-            return super().get_queryset().defer("number")
         return super().get_queryset()
--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from core.managers import ChapterManager
-from core.blue_green_utils import is_migration_applied
 
 
 class Book(models.Model):
@@ -15,22 +14,9 @@ class Chapter(models.Model):
     book = models.ForeignKey('Book',
                              on_delete=models.CASCADE,
                              related_name='chapters')
-    number = models.PositiveSmallIntegerField(null=True)
     name = models.CharField(max_length=200)
 
     objects = ChapterManager()
 
     def __str__(self):
-        return f'{self.book}, Chapter {self.temp_number}: {self.name}'
-
-    @property
-    def temp_number(self):
-        if is_migration_applied():
-            return None
-        return self.number
-
-    @temp_number.setter
-    def temp_number(self, value):
-        if is_migration_applied():
-            return
-        self.number = value
+        return f'{self.book}, Chapter: {self.name}'
